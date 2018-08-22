b44df36e939a9eb07c243693ab3d9561d3fa7dc7
Rendon Elsesser <rendonelsesser@gmail.com>
2018-08-22 21:37:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ logWhisper("THIS WAS A SHOUTING COMMAND")
 function sayHiToGrandma(string) {
  if (string === string.toLowerCase()) return "I can't hear you!"
  if (string === string.toUpperCase()) return "YES INDEED!"
+ if (string === "I love you, Grandma.") return "I love you, too."
 }
 
 
