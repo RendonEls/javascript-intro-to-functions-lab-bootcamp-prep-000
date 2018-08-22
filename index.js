39207f9471fe61a9b2ca4c35d26cbd977f2e227f
Rendon Elsesser <rendonelsesser@gmail.com>
2018-08-22 21:23:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function whisper(string) {
 whisper("THIS SHOULD BE IN CAPS BUT NOW LOWERCASE")
 
 function logShout(string) {
-  return console.log(string.toUpperCase))
+  return console.log(string.toUpperCase()))
 }
 logShout("this is a whisper turned into a shout")
