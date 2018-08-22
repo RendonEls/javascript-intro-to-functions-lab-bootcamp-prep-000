aba65f8715fe937f6c516dcf365a64c0ce8d68cc
Rendon Elsesser <rendonelsesser@gmail.com>
2018-08-22 21:20:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function shout(string) {
   return string.toUpperCase()
-}
+};
+
+shout("Hello");
+
 function whisper(string) {
   return string.toLowerCase()
 }
+
+whisper("THIS SHOULD BE IN CAPS BUT NOW LOWERCASE")
+
 function logShout(string) {
   return console.log
 }
